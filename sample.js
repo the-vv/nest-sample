8d836a3ca1e7cfbe0a6898202fc0c23feb6403e1
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2022-07-19 22:27:17 +0530

    something

--- sample.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello World');
-
-console.log(12345565);
-
-console.log(12345);
-
-
-
-console.log('version 1.0.0');
--- sample2.js
+++ sample2.js
@@ -1,10 +1,2 @@
-console.log('Hello World2');
+console.log(12345)
 
-
-console.log(1234545646567567)
-
-
-console.log(12345);
-
-
-console.log('this is new version')
